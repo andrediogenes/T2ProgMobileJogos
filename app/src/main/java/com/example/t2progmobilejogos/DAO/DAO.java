e799ee25eef9046d8888ad037c9b6295aca13184
andre.diogenes <andre.diogenes@meta.com.br>
2022-07-01 16:04:57 -0300

    criando a tabela time e jogador

--- app/src/main/java/com/example/t2progmobilejogos/DAO/DAO.java
+++ app/src/main/java/com/example/t2progmobilejogos/DAO/DAO.java
@@ -1,4 +1,46 @@
 package com.example.t2progmobilejogos.DAO;
 
-public class DAO {
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAO extends SQLiteOpenHelper {
+    public DAO(Context context) {
+        super(context, "USUARIO", null, 1);
+    }
+
+    //Criacao das tabelas no banco
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql_jogador = "CREATE TABLE JOGADOR (JOGADOR_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "JOGADOR_IDTIME INTEGER," +
+                "JOGADOR_NOME TEXT," +
+                "JOGADOR_CPF TEXT," +
+                "JOGADOR_NASC INT);";
+        String sql_time = "CREATE TABLE TIME (TIME_ID INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "TIME_DESCRICAO INTEGER);";
+
+        db.execSQL(sql_jogador);
+        db.execSQL(sql_time);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versaoAntiga, int versaoNova) {
+        String sql_jogador = "DROP TABLE IF EXISTS JOGADOR;";
+        String sql_time = "DROP TABLE IF EXISTS TIME;";
+
+        db.execSQL(sql_jogador);
+        db.execSQL(sql_time);
+
+        onCreate(db);
+    }
 }
